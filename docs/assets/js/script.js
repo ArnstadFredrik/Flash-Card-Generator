50a618d097e69de6428e6dea1a5c9847688ca152
Fredrik Arnstad <fredrikarnstad@icloud.com>
2020-01-28 21:45:28 +0100

    frontend redesign and options for sorting option, font and border

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,17 +1,76 @@
-console.log('Hello There');
-
 Array.prototype.move = function(from, to) {
     this.splice(to, 0, this.splice(from, 1)[0]);
 };
 
 const word = {}
+word.setting = 'tab'
+word.font = 'hebrew'
+word.cssFontFamily = function () {
+  switch (this.font) {
+    case 'hebrew':
+      return 'SBL Hebrew'
+      break;
+    case 'greek':
+      return 'SBL Greek'
+      break;
+    case 'norwegian':
+      return "sans-serif"
+      break;
+  }
+}
+word.cssFontSize = function () {
+  switch (this.font) {
+    case 'hebrew':
+      return '2.5rem'
+      break;
+    case 'greek':
+      return '2rem'
+      break;
+    case 'norwegian':
+      return "1.5rem"
+      break;
+  }
+}
+word.border = false
+word.cssBorder = function () {
+  switch (this.border) {
+    case true:
+      return '1px solid lightgray'
+      break;
+    case false:
+      return '1px solid transparent'
+      break;
+  }
+}
+word.getSettings = function () {
+  const radioButtons = document.getElementsByName('selectOpt')
+
+  for (button of radioButtons){
+    if (button.checked) {
+      this.setting = button.value
+      return this.setting
+    }
+  }
+
+}
 word.createLines = function () {
   this.arrayLines = this.input.split('\n')
 }
 word.createList = function () {
   this.list = []
+  let splitOption
+  switch (this.setting) {
+    case 'tab':
+      splitOption = '\t'
+      break;
+    case 'comma':
+      splitOption = ','
+      break;
+  }
+
   for (let i = 0; i < this.arrayLines.length; i++) {
-    this.list.push(this.arrayLines[i].split('\t'))
+    this.list.push(this.arrayLines[i].split(splitOption))
+    console.log(this.list);
   }
 }
 word.createLanguageList = function () {
@@ -24,24 +83,52 @@ word.createLanguageList = function () {
       this.norwegian.push(x[1])
     }
   }
-  if (this.hebrew.length % 2 != 0) {
+
+  if (this.hebrew.length % 2 != 0 || this.hebrew.length == 1) {
     this.hebrew.push('')
   }
-  if (this.norwegian.length % 2 != 0) {
+  if (this.norwegian.length % 2 != 0 || this.norwegian.length == 1) {
     this.norwegian.push('')
   }
+
   let length = this.norwegian.length
-  for (let i = 0; i < length; i++) {
-    if (i % 2 == 0) {
-      this.norwegian.move(i,i+1)
+    for (let i = 0; i < length; i++) {
+      if (i % 2 == 0) {
+        this.norwegian.move(i,i+1)
+      }
     }
+
+    console.log(this.hebrew);
+    console.log(this.norwegian);
+}
+word.createArray = function () {
+  let length = Math.ceil(this.hebrew.length/12)
+  let heb = this.hebrew
+  let nor = this.norwegian
+
+  for (let i = 1; i <= length; i++) {
+    let arrayH
+    let arrayN
+    if (this.hebrew.length < 12) {
+      arrayH = heb.splice(0,this.hebrew.length)
+      arrayN = nor.splice(0,this.norwegian.length)
+    }
+    else {
+      arrayH = heb.splice(0,12)
+      arrayN = nor.splice(0,12)
+    }
+    this.hebrew = arrayH
+    this.norwegian = arrayN
+    this.createPage(arrayH,arrayN)
   }
+
 }
 word.wrapper = document.querySelector('.cardWrapper')
-
 word.createPage = function (arrayH,arrayN) {
   let hebrew = document.createElement('div')
   hebrew.className = 'page hebrew'
+  hebrew.style.fontFamily = this.cssFontFamily()
+  hebrew.style.fontSize = this.cssFontSize()
   let norwegian = document.createElement('div')
   norwegian.className = 'page norwegian'
 
@@ -53,10 +140,12 @@ word.createPage = function (arrayH,arrayN) {
   for (let i = 0; i < arrayH.length; i++) {
     const hebElement = document.createElement('div')
     hebElement.className = 'card hebrew'
+    hebElement.style.border = this.cssBorder()
     hebElement.innerText = arrayH[i]
 
     const norElement = document.createElement('div')
     norElement.className = 'card norwegian'
+    norElement.style.border = this.cssBorder()
     norElement.innerText = arrayN[i]
 
     hebrew.appendChild(hebElement)
@@ -70,35 +159,59 @@ word.createPage = function (arrayH,arrayN) {
   this.wrapper.appendChild(hebWrap)
   this.wrapper.appendChild(norWrap)
 }
-word.createArray = function () {
-  let length = Math.ceil(this.list.length/12)
-  for (let i = 1; i <= length;i++) {
-    console.log(i);
-    let arrayH
-    let arrayN
-    if (this.list.length < 12) {
-      //console.log(this.lis2t.splice(0,this.list.length));
-      arrayH = this.hebrew.splice(0,this.list.length)
-      arrayN = this.norwegian.splice(0,this.list.length)
-    }
-    else {
-      //console.log(this.list.splice(0,8));
-      arrayH = this.hebrew.splice(0,12)
-      arrayN = this.norwegian.splice(0,12)
-    }
-    console.log(arrayH,arrayN);
-    this.createPage(arrayH,arrayN)
 
-  }
-}
-let input
-const generateBtn = document.querySelector('.generate')
-generateBtn.addEventListener('click',(e) => {
+word.processInput = function () {
   word.input = document.querySelector('.gloserInput').value
-  word.createLines(word.input)
-  word.createList(word.arrayLines)
-  word.createLines(word.input)
+  //word.getSettings()
+  word.createLines()
+  word.createList()
   word.createLanguageList()
   word.createArray()
-  //console.log(word);
+}
+word.reset = function () {
+  // this.input = ""
+  // this.norwegian = []
+  // this.hebrew = []
+  // this.list = []
+  // this.arrayLines = []
+  this.wrapper.innerHTML = ""
+}
+const generateBtn = document.querySelector('.generate')
+generateBtn.addEventListener('click',(e) => {
+  word.reset()
+  word.processInput()
+  window.print()
+})
+
+const sortButtons = document.querySelectorAll('.sort')
+for (x of sortButtons) {
+  x.addEventListener('click',(e) => {
+    sortButtons.forEach(btn => {
+      btn.classList.remove('checked')
+    })
+    e.target.classList.toggle('checked')
+    word.setting = e.target.dataset.value
+  })
+}
+
+const fontButtons = document.querySelectorAll('.font')
+for (x of fontButtons) {
+  x.addEventListener('click',(e) => {
+    fontButtons.forEach(btn => {
+      btn.classList.remove('checked')
+    })
+    e.target.classList.toggle('checked')
+    word.font = e.target.dataset.value
+  })
+}
+
+const borderButton = document.querySelector('.border')
+  borderButton.addEventListener('click',(e) => {
+    e.target.classList.toggle('checked')
+    if (e.target.classList.contains('checked')) {
+      word.border = true
+    }
+    else {
+      word.border = false
+    }
 })
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-This is a flash card generator I made for my hebrew studies. The generator sorts by tab, so copying from excel or equivalent.
+## Flash Card Generator
+This is a flash card generator I made for my hebrew studies.
+- Options for sorting by comma or tab (tab i default)
+- Option for font selection. Hebrew or greek fonts and standard sans-serif for "Norwegian" (hebrew is default)
+- Option for adding border around each card (default no border)
 
-Feel free to use it for other things as well.
+The flash cards are generated in HTML and can be saved from the print dialog
